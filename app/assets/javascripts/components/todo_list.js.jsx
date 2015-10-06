80e85e14c1718e9f19878e29c86342cc4a0d4b26
Patrick Sandquist <patricksandquist@gmail.com>
2015-10-06 16:00:20 -0700

    Add form to make new todos

--- app/assets/javascripts/components/todo_list.js.jsx
+++ app/assets/javascripts/components/todo_list.js.jsx
@@ -18,6 +18,7 @@
           {this.state.todoList.map(function(todoItem) {
             return <root.TodoListItem todoItem={todoItem} key={todoItem.id}/>;
           })}
+          <root.TodoForm/>
         </div>
       );
     },
@@ -38,6 +39,39 @@
           <div>{this.props.todoItem.title}</div>
           <div>{this.props.todoItem.body}</div>
         </div>
+      );
+    }
+  });
+
+  root.TodoForm = React.createClass({
+    // mixins: [React.addons.LinkedStateMixin],
+
+    getInitialState: function() {
+      return {title: "", body: ""};
+    },
+
+    createTodo: function(e) {
+      e.preventDefault();
+      var todoObject = {title: this.state.title, body: this.state.body};
+      root.TodoStore.create(todoObject);
+      this.setState({title: "", body: ""});
+    },
+
+    handleTitleChange: function(e) {
+      this.setState({title: e.target.value});
+    },
+
+    handleBodyChange: function(e) {
+      this.setState({body: e.target.value});
+    },
+
+    render: function () {
+      return (
+        <form onSubmit={this.createTodo}>
+          <input type='text' value={this.state.title} onChange={this.handleTitleChange}></input>
+          <input type='text' value={this.state.body} onChange={this.handleBodyChange}></input>
+          <input type='submit' value='Create todo!'></input>
+        </form>
       );
     }
   });
